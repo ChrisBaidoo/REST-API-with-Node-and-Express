3374684622cc5cd6b3e608a02b85843ebfc05762
christian baidoo <chris_pens@hotmail.co.uk>
2020-03-28 05:07:41 +0000

    initial commit

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,35 @@
+import {
+    addNewContact,
+    getContacts,
+    getContactWithID,
+    updateContact,
+    deleteContact
+  } from "../controller/crmController";
+  
+  const routes = app => {
+    app
+      .route("/contact")
+      .get((req, res, next) => {
+        //middleware
+        console.log(`Request from: ${req.originalUrl}`);
+        console.log(`Request from: ${req.method}`);
+        next();
+      }, getContacts)
+  
+      //post endpoint
+      .post(addNewContact);
+  
+    app
+      .route("/contact/:contactID")
+      //get a specific contact
+      .get(getContactWithID)
+  
+      //updating a specific contact
+      .put(updateContact)
+  
+      //deleting a specific contact
+      .delete(deleteContact);
+  };
+  
+  export default routes;
+  
